48ea1964294bc559bdb81d3ca10e3ba530718e59
Rog <rog@hatbat.net>
2025-12-16 09:51:19 +0000

    Initial Commit

---
+++ JonesControlTests/JonesControlTests.swift
@@ -0,0 +1,17 @@
+//
+//  JonesControlTests.swift
+//  JonesControlTests
+//
+//  Created by Roger Nolan on 16/12/2025.
+//
+
+import Testing
+@testable import JonesControl
+
+struct JonesControlTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
